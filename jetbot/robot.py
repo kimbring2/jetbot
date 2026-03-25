844c5a2b643f985a213e136486addb9b5a0ed28f
kimbring2 <kimbring2@gmail.com>
2026-03-25 12:41:25 +0900

    sim to real update

--- jetbot/robot.py
+++ jetbot/robot.py
@@ -44,16 +44,14 @@ from .motor import Motor
 addresses = get_i2c_address(7)
 
 class Robot(SingletonConfigurable):
-
     left_motor = traitlets.Instance(Motor)
     right_motor = traitlets.Instance(Motor)
     
-    
     # config
     i2c_bus = traitlets.Integer(default_value=7).tag(config=True)
-    left_motor_channel = traitlets.Integer(default_value=1).tag(config=True)
+    left_motor_channel = traitlets.Integer(default_value=2).tag(config=True)
     left_motor_alpha = traitlets.Float(default_value=1.0).tag(config=True)
-    right_motor_channel = traitlets.Integer(default_value=2).tag(config=True)
+    right_motor_channel = traitlets.Integer(default_value=1).tag(config=True)
     right_motor_alpha = traitlets.Float(default_value=1.0).tag(config=True)
 
     # Adafruit Hardware
@@ -65,28 +63,44 @@ class Robot(SingletonConfigurable):
             self.right_motor = Motor(self.motor_driver, channel=self.right_motor_channel, alpha=self.right_motor_alpha)
             
         def set_motors(self, left_speed, right_speed):
-            self.left_motor.value = left_speed
+            self.left_motor.value = -left_speed
             self.right_motor.value = right_speed
             
         def forward(self, speed=1.0, duration=None):
-            self.left_motor.value = speed
-            self.right_motor.value = -speed
+            self.left_motor.value = -speed
+            self.right_motor.value = speed
 
         def backward(self, speed=1.0):
-            self.left_motor.value = -speed
-            self.right_motor.value = speed
-
-        def left(self, speed=1.0):
-            self.left_motor.value = -speed
+            self.left_motor.value = speed
             self.right_motor.value = -speed
 
-        def right(self, speed=1.0):
+        def left(self, speed=1.0):
             self.left_motor.value = speed
             self.right_motor.value = speed
+
+        def right(self, speed=1.0):
+            self.left_motor.value = -speed
+            self.right_motor.value = -speed
 
         def stop(self):
             self.left_motor.value = 0
             self.right_motor.value = 0
+
+        def get_forward_speed(self, K_v=0.65):
+            # 'forward' in your class uses -left and +right
+            # We normalize these to get the net forward component
+            left_pwm = -self.left_motor.value  # Convert back to positive for forward
+            right_pwm = self.right_motor.value
+
+            print("left_pwm: ", left_pwm)
+            print("right_pwm: ", right_pwm)
+            print("")
+            
+            # This is the real-world equivalent of root_com_lin_vel_b[:,0]
+            forward_speed = ((left_pwm + right_pwm) / 2.0) * K_v
+            
+            return forward_speed
+            
 
     # SparkFun Hardware
     elif 93 in addresses:
